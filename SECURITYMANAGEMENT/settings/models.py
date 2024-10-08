1b39853d9953d99f4a5517bf38bce2bcce5f1bdd
sowmyanaidu1117 <Lakshmi.Sowmya@ascenddefi.com>
2024-10-08 16:46:49 +0530

    added backend for security management

---
+++ SECURITYMANAGEMENT/settings/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+import qrcode
+from io import BytesIO
+import base64
+from django.core.validators import RegexValidator
+from django.db.models import Max
+import random
+
+class Notificationthings(models.Model):
+    user_id = models.CharField(max_length=255, unique=True, primary_key=True)
+    product_announcement = models.BooleanField(default=True)
+    insights_tips = models.BooleanField(default=True)
+    special_offers = models.BooleanField(default=True)
+    price_alerts = models.BooleanField(default=True)
+    account_activity = models.BooleanField(default=True)
+    messages = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'notification_settings'
+        managed = False
+
+
+class Password(models.Model):
+    id = models.CharField(max_length=20, primary_key=True)
+    password_creation = models.CharField(max_length=255)
+    retype_password = models.CharField(max_length=225)
+
+    class Meta:
+        db_table = 'app_password'
+        managed = False
+        
---
+++ SECURITYMANAGEMENT/settings/urls.py
@@ -0,0 +1,27 @@
+from django import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import FetchQRCodeView, ProjectViewSet,CustomUserViewSet
+from .views import NotificationViewSet
+from .views import PasswordViewSet, RepasswordViewSet,  LogPassword, LogPasswordLock, GenerateOTP, verify_otp , RecreatePasscode
+from settings import views
+
+router = DefaultRouter()
+
+router.register(r'profile',CustomUserViewSet,basename="myProfile")
+router.register(r'notification', NotificationViewSet, basename='notification')
+router.register(r'password', PasswordViewSet, basename='password')
+router.register(r'repassword', RepasswordViewSet, basename='repassword')
+router.register(r'logpassword', LogPassword, basename='logpassword')
+router.register(r'logpassword1', LogPasswordLock, basename='logpassword1')
+# router.register(r'verify_otp', verify_otp, basename='verify_otp')
+router.register(r'recreatepasscode', RecreatePasscode, basename='recreatepasscode')
+
+
+urlpatterns = [
+    path('',include(router.urls)),
+    path('generate_otp/', GenerateOTP.as_view(), name='generate_otp'),
+    path('verify_otp/', verify_otp, name='verify_otp'),
+    # path('profile/<pk>/', UserProfileView.as_view())
+    
+]
